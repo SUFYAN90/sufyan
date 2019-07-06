5d1bd4a63cef3797603c96f526f6a796e512d224
SUFYAN90 <52576887+SUFYAN90@users.noreply.github.com>
2019-07-06 09:12:27 +0600

    Add files via upload

---
+++ work7.js
@@ -0,0 +1,67 @@
+$(document).ready(function() { 
+	$("body").scrollTop > 100(function() { 
+		alert("scroll happened"); 
+	}); 
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+new WOW().init();
+var wow = new WOW(
+  {
+    boxClass:     'wow',      
+    animateClass: 'animated', 
+    offset:       0,          
+    mobile:       true,       
+    live:         true,       
+    callback:     function(box) {
+      
+    },
+    scrollContainer: null,    
+    resetAnimation: true,     
+  }
+);
+wow.init();
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+
+
+
+
+//sufyan-khan.github.io
+
+//https://www.youtube.com/watch?v=cNcpKvHEp-w
+
+
+
+
+	
+
